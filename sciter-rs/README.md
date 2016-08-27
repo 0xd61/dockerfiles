37171a6feb16cc86f86bfeb21273b52a5d01e7ff
Kaitsh <kaitsh@d-git.de>
2016-08-27 15:45:18 +0200

    install required packages

--- sciter-rs/README.md
+++ sciter-rs/README.md
@@ -1,11 +1,11 @@
-# rust
+# sciter-rs
 
-Lightweight nightly Rust build including Cargo and GDB
+Lightweight rust environment with the sciter library.
 
 ## Usage
 
 ```sh
-$ docker run --rm -it -v $(pwd):/source schickling/rust
+$ docker run --name sciter-rs -it -e DISPLAY -v $(pwd):/source:Z kaitsh/sciter-rs
 ```
 
 Mount your local source folder to the container and compile & run code inside.
@@ -23,6 +23,6 @@ $ ./hello-world
 The `$USER` environment variable can be specified, which is used by `cargo`. The default is `root`.
 
 ```sh
-$ docker run --rm -it -e USER="John Doe" -v $(pwd):/source schickling/rust
+$ docker run --rm -it -e USER="John Doe" -v $(pwd):/source kaitsh/sciter-rs
 ```
 
--- sciter-rs/install.sh
+++ sciter-rs/install.sh
@@ -8,6 +8,9 @@ triple=x86_64-unknown-linux-gnu
 # install curl (needed to install rust)
 apt-get update && apt-get install -y curl gdb g++-multilib lib32stdc++6 libssl-dev libncurses5-dev
 
+# install misc apps
+apt-get install -y wget vim
+
 # install rust
 curl -sL https://static.rust-lang.org/dist/rust-nightly-$triple.tar.gz | tar xvz -C /tmp
 /tmp/rust-nightly-$triple/install.sh
@@ -16,9 +19,19 @@ curl -sL https://static.rust-lang.org/dist/rust-nightly-$triple.tar.gz | tar xvz
 curl -sL https://static.rust-lang.org/cargo-dist/cargo-nightly-$triple.tar.gz | tar xvz -C /tmp
 /tmp/cargo-nightly-$triple/install.sh
 
+# install gtk
+apt-get install -y libgtk-3-dev
+
 # cleanup package manager
-apt-get remove --purge -y curl && apt-get autoclean && apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+apt-get autoclean && apt-get clean # remove --purge -y curl && apt-get autoclean && apt-get clean
+#rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# install sciter lib
+wget https://github.com/c-smile/sciter-sdk/raw/master/bin.gtk/libsciter-gtk-64.so
+mv libsciter-gtk-64.so /usr/lib
+echo '/usr/lib' > /etc/ld.so.conf.d/libsciter.conf
+ldconf
+
 
 # prepare dir
 mkdir /source
